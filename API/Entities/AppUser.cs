4ec315b85daea287625e57fcc2de95d9101c2657
¨Meepster777¨ <56267888+Meepster777@users.noreply.github.com>
2023-11-10 09:09:47 -0600

    Seccion 08 - Manejo de EF

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -6,5 +6,21 @@ public class AppUser
     public string UserName { get; set; }
     public byte[] PasswordHash { get; set; }
     public byte[] PasswordSalt { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+    public string KnownAs { get; set; }
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+    public DateTime LastActive { get; set; } = DateTime.UtcNow;
+    public string Gender { get; set; }
+    public string Introduction { get; set; }
+    public string LookingFor { get; set; }
+    public string Interests { get; set; }
+    public string City { get; set; }
+    public string County { get; set; }
+    public List<Photo> Photos { get; set; } = new();
+
+    //    public int DameLaEdad()
+    //    {
+    //        return DateOfBirth.CalculateAge();
+    //    }
 
 }
